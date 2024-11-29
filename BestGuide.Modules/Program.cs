62fca70fe429e09b222706e38657de5c4de3742d
Tahir Köşkeroğlu <ttkoskeroglu@gmail.com>
2024-11-29 19:55:32 +0300

    project architecture designed as modules and report microservices

---
+++ BestGuide.Modules/Program.cs
@@ -0,0 +1,50 @@
+using BestGuide.Modules.Infrastructure.Persistence;
+using MediatR;
+using MediatR.Pipeline;
+using System.Reflection;
+
+namespace BestGuide.Modules
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDatabaseContext();
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPostProcessorBehavior<,>));
+            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPreProcessorBehavior<,>));
+            builder.Services.AddMediatR(options => options.RegisterServicesFromAssemblies(Application.Meta.Assembly));
+
+            builder.Services.AddAutoMapper(options =>
+            {
+                options.AllowNullCollections = true;
+            }, Assembly.GetExecutingAssembly());
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
